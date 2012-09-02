835b668a84ec1d603430c86793dae76cf6a8422d
Fahim Chowdhury <fahim@gmx.com>
2012-09-02 14:02:05 -0700

    created a button component

--- src/com/view/View.js
+++ src/com/view/View.js
@@ -14,30 +14,20 @@ var View = function() {
 		label.className("label");
 		label.width(230);
 		label.text("This is a Label for a Vertical Layout");
-		
 		this.addChild(label);
-		
-		var sprite2 = new UIElement();
-		sprite2.build();
-		sprite2.className("box2");
-		sprite2.width(100);
-		sprite2.height(100);
-		sprite2.visible(true);
-		sprite2.buttonMode(true);
-		
-		
-		this.addChild(sprite2);
 
-		var sprite3 = new UIElement();
-		sprite3.build();
-		sprite3.className("box3");
-		sprite3.width(100);
-		sprite3.height(100);
-		sprite3.visible(true);
-		sprite3.buttonMode(true);
-
-		this.addChild(sprite3);
-		
+		var button = new Button();
+		button.build();
+		button.className("button");
+		button.asset("resource/image/button.png");
+		button.width(150);
+		button.height(30);
+		button.labelClassName("buttonLabel");
+		button.setStyle();
+		this.addChild(button);
+		button.activate();
+	//	button.deactivate();
+		button.text("Next");
 		
 	}
 	this.arrange=function()
