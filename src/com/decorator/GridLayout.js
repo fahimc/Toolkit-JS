c7c876bc821fe6d9631784ad27e2fd9f73dbf3b0
Fahim Chowdhury <fahim@gmx.com>
2012-09-22 16:26:46 -0700

    scroller complete

--- src/com/decorator/GridLayout.js
+++ src/com/decorator/GridLayout.js
@@ -1,6 +1,6 @@
 var GridLayout = function() {
 	this.arrange = function(e) {
-		var obj = e.childContainer;
+		var obj = e.wrapper?e.wrapper:e.childContainer;
 		var currentX = this.left;
 		var currentY = this.top;
 		var col = 0;
@@ -9,6 +9,9 @@ var GridLayout = function() {
 		this.clearGrid(obj);
 		for(var count = 0; count < obj.childNodes.length; count++) {
 			var child = obj.childNodes[count];
+			if(child.className.indexOf("scroll")<0 && child.className.indexOf("mcontentwrapper")<0)
+			{
+				
 			var x = parseInt(child.style.left.replace("px", "") ? child.style.left.replace("px", "") : 0);
 			var y = parseInt(child.style.top.replace("px", "") ? child.style.top.replace("px", "") : 0);
 
@@ -31,6 +34,7 @@ var GridLayout = function() {
 			child.setAttribute("gridRow", row);
 			currentX += parseInt(child.clientWidth) + parseInt(this.horizontalGap);
 			col++;
+			}
 		}
 	}
 	this.getChildHeight = function(obj, r, c) {
