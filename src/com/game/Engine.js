ce35ca294fbd65edb4afbb4e4dd424c984bf9a67
unknown <Fahim Chowdhury@.(none)>
2013-02-17 20:26:25 +0000

    working on path finder

--- src/com/game/Engine.js
+++ src/com/game/Engine.js
@@ -42,7 +42,7 @@ var Engine = function() {
 			sp.setStyle();
 			holder.addChild(sp);
 			sp.arrange();
-			sp.display.id = this.prop.gridCellId + currentCol+"_"+currentRow;
+			sp.display.id = this.prop.gridCellId + currentCol + "_" + currentRow;
 			this.prop.gridCellIndex++;
 			currentCol++;
 			if (currentCol >= cols) {
@@ -56,32 +56,30 @@ var Engine = function() {
 		holder.arrange();
 		document.body.appendChild(holder.display);
 	};
-	this.setCell=function(x,y)
-	{
+	this.setCell = function(x, y,color) {
 		if (!this.prop.grid)
 			return;
-		var id= this.prop.gridCellId + x+"_"+y;
+		var id = this.prop.gridCellId + x + "_" + y;
 		var cell = document.getElementById(id);
-		cell.style.backgroundColor="#f00";
+		cell.style.backgroundColor = color?color:"#f00";
 	};
-	this.addObstacle=function(obj,y) {
-		var startX = this.getXCell(y==undefined?obj.x():obj);
-		var startY = this.getYCell(y==undefined?obj.y():y);
-		var endX = this.getXCell(y==undefined?(Int(obj.x())+Int(obj.width())):obj);
-		var endY = this.getYCell(y==undefined?(Int(obj.y())+Int(obj.height())):y);
-		var end=false;
-		var cX=startX;
-		while(!end)
-		{
-			this.setOccupiedXY(cX,startY);
-			this.setCell(cX,startY);
+	this.addObstacle = function(obj, y) {
+		var startX = this.getXCell(y == undefined ? obj.x() : obj);
+		var startY = this.getYCell(y == undefined ? obj.y() : y);
+		var endX = this.getXCell(y == undefined ? (Int(obj.x()) + Int(obj.width())) : obj);
+		var endY = this.getYCell(y == undefined ? (Int(obj.y()) + Int(obj.height())) : y);
+		var end = false;
+		var cX = startX;
+		while (!end) {
+			this.setOccupiedXY(cX, startY);
+			this.setCell(cX, startY);
 			cX++;
-			if(cX>=endX)
-			{
-				cX=startX;
+			if (cX >= endX) {
+				cX = startX;
 				startY++;
 			}
-			if(startY>=endY)end=true;
+			if (startY >= endY)
+				end = true;
 		}
 		delete startX;
 		delete startY;
@@ -91,66 +89,140 @@ var Engine = function() {
 		delete end;
 		console.log(this.prop.occupiedXY);
 	};
-	this.setOccupiedXY=function(x,y)
-	{
-		if(!this.prop.occupiedXY[x])this.prop.occupiedXY[x]=[];
-		this.prop.occupiedXY[x][y]=x+","+y;
+	this.setOccupiedXY = function(x, y) {
+		if (!this.prop.occupiedXY[x])
+			this.prop.occupiedXY[x] = [];
+		this.prop.occupiedXY[x][y] = x + "," + y;
 	};
-	this.getOccupiedXY=function(x,y)
-	{
-		var startX ;
-		var startY ;
-		if(x.x)
-		{
+	this.getOccupiedXY = function(x, y) {
+		var startX;
+		var startY;
+		if (x.x) {
 			startX = this.getXCell(x.x());
 			startY = this.getYCell(x.y());
-			var endX = this.getXCell(Int(x.x())+Int(x.width()));
-			var endY = this.getYCell(Int(x.y())+Int(x.height()));
-			var cX=startX;
-			var occupied=false;
-			var end=false;
-			while(!end)
-			{
-			if(this.prop.occupiedXY[cX] && this.prop.occupiedXY[cX][startY])occupied= true;
-			cX++;
-			if(cX>=endX)
-			{
-				cX=startX;
-				startY++;
+			var endX = this.getXCell(Int(x.x()) + Int(x.width()));
+			var endY = this.getYCell(Int(x.y()) + Int(x.height()));
+			var cX = startX;
+			var occupied = false;
+			var end = false;
+			while (!end) {
+				if (this.prop.occupiedXY[cX] && this.prop.occupiedXY[cX][startY])
+					occupied = true;
+				cX++;
+				if (cX >= endX) {
+					cX = startX;
+					startY++;
+				}
+				if (startY > endY)
+					end = true;
 			}
-			if(startY>endY)end=true;
-			}
-			if(occupied)return true;
-		}else{
+			if (occupied)
+				return true;
+		} else {
 			startX = this.getXCell(x);
 			startY = this.getYCell(y);
-			if(this.prop.occupiedXY[startX] && this.prop.occupiedXY[startX][startY])return true;
+			if (this.prop.occupiedXY[startX] && this.prop.occupiedXY[startX][startY])
+				return true;
 		}
-		
+
 		return false;
 	};
-	this.findPathTo=function(obj,x,y)
-	{
+	this.findPathTo = function(obj, x, y) {
 		var startX = this.getXCell(obj.x());
 		var startY = this.getYCell(obj.y());
-		
+
 		var endX = this.getXCell(x);
 		var endY = this.getYCell(y);
-		
-		var v = startY>endY?"up":"down";
-		var h = startX>endX?"left":"right";
-		var md = Math.abs(startX-endX)>Math.abs(startY-endY)?"h":"v";
-		
-		console.log(md,v,h)
-		
-	}
-	this.getXCell=function(x)
-	{
-		return Math.floor(x/this.cellSize());
+
+		var v = startY > endY ? "up" : "down";
+		var h = startX > endX ? "left" : "right";
+		var md = Math.abs(startX - endX) > Math.abs(startY - endY) ? "h" : "v";
+
+		console.log(startX,startY,endX,endY);
+
+		var paths = [];
+		var cy = startY;
+		var cx = startX;
+		var end = false;
+		while (!end) {
+			if(startY<endY)
+				{
+					cy++;
+				}else{
+					cy--;
+				}
+				if(startX<endX)
+				{
+					cx++;
+				}else{
+					cx--;
+				}
+			if (md == "v") {
+				
+				if (this.prop.occupiedXY[cx] && this.prop.occupiedXY[cx][cy]) {
+					//check if x-1 is has object or check if x+1 has object
+					var hasLeft = (this.prop.occupiedXY[cx - 1] && this.prop.occupiedXY[cx-1][cy]) ? true : false;
+					var hasRight = (this.prop.occupiedXY[cx + 1] && this.prop.occupiedXY[cx+1][cy]) ? true : false;
+					if (!hasLeft) {
+						cx--;
+					} else if (!hasRight) {
+						cx++;
+					}else{
+						md="h";
+					}
+					console.log("v has",hasLeft,hasRight,cx+1,cx);
+				}
+					
+			}
+			if (md == "h") {
+				if (this.prop.occupiedXY[cx] && this.prop.occupiedXY[cx][cy]) {
+					//check if x-1 is has object or check if x+1 has object
+					var hasUp = (this.prop.occupiedXY[cx] && this.prop.occupiedXY[cx][cy-1]) ? true : false;
+					var hasDown = (this.prop.occupiedXY[cx] && this.prop.occupiedXY[cx][cy+1]) ? true : false;
+					if (!hasUp) {
+						cy--;
+					} else if (!hasDown) {
+						cy++;
+					}else{
+						md="v";
+					}
+					console.log("h has",hasUp,hasDown,cx+1,cx)
+				}
+			}
+			paths.push([cx, cy]);
+			var xEnded=false;
+			var yEnded=false;
+			if(startX<endX)
+			{
+				if(cx >=endX)xEnded=true;
+			}else{
+				if(cx <=endX)xEnded=true;
+			}
+			if(startY<endY)
+			{
+				if(cy >=endY)yEnded=true;
+			}else{
+				if(cy <=endY)yEnded=true;
+			}
+			if(xEnded && yEnded)end=true;
+		}
+		console.log(paths);
+		this.drawPath(paths);
 	};
-	this.getYCell=function(y)
+	this.drawPath=function(paths)
 	{
-		return Math.floor(y/this.cellSize());
+		if (!this.prop.grid)
+			return;
+		for(var a=0;a<paths.length;a++)
+		{
+			this.setCell(paths[a][0],paths[a][0],"#00f");
+		}
+	}
+	this.getXCell = function(x) {
+		return Math.floor(x / this.cellSize());
+	};
+	this.getYCell = function(y) {
+		return Math.floor(y / this.cellSize());
 	};
 	this.cellSize = function(value) {
 		if (value == undefined) {
