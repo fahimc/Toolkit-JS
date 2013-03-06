97963241437ea834314d418a3b9ab969046996d0
unknown <fahim.chowdhury@.omega.wwads.net>
2013-03-06 15:35:25 +0000

    tutorials added

--- examples/lib/fahimchowdhury/toolkit/test/View.js
+++ examples/lib/fahimchowdhury/toolkit/test/View.js
@@ -1,27 +1,24 @@
-var Vehicle = function()
+var Vehicle = function(){};
+
+Vehicle.prototype =
 {
+	wheels:0
+};
 
-}
+var Car = function(){};
 
-Vehicle.prototype = {
-wheels:0,
-	}
-
-var Car = function()
+(function()
 {
+	var _ = Class.extend(Car,Vehicle);
+	_.wheels=4;
+})();
 
-}
+var Bike = function(){};
 
-var _ = Class.extend(Car,Vehicle);
-
-_.wheels = 4;
-
-
- var Bike = function()
+(function()
 {
+	var _ = Class.extend(Bike,Vehicle);
+	_.wheels = 2;
+})();
 
-}
 
-var _ = Class.extend(Bike,Vehicle);
-
-_.wheels =2;
