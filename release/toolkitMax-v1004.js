72e1d18bb2bb412309637f0c2ad30498775ffafb
unknown <Fahim@Fahim-PC.(none)>
2013-03-13 22:10:32 +0000

    added a ie7 fixed and contextmenu tutorial

--- release/toolkitMax-v1004.js
+++ release/toolkitMax-v1004.js
@@ -548,7 +548,7 @@ var Event =
 		var event = {
 			type : type,
 			target : target,
-			args:args,
+			args:args
 		};
 		var args = [];
 		var numOfArgs = arguments.length;
@@ -2263,3 +2263,114 @@ else
 }
 
 };
+var IIterator = {
+	reset : function() {
+	},
+	next : function() {
+	},
+	hasNext : function() {
+	},
+	index : function() {
+	},
+	purge : function() {
+	},
+	length : function() {
+	}
+}
+var ArrayIterator =function(collection){
+	this._index = 0;
+	this._collection=collection;
+	this.reset =function()
+	{
+		this._index = 0;
+	};
+	this.next =function()
+	{
+		return this._collection[this._index++];
+	};
+	this.hasNext =function()
+	{
+		return this._index < this._collection.length;
+	};
+	this.length =function()
+	{
+		return this._collection.length;
+	};
+	this.index =function()
+	{
+		return this._index;
+	};
+	this.purge =function()
+	{
+		delete this._collection;
+	};
+	
+	
+};
+Class.implement(ArrayIterator,IIterator);
+var Collection = function(arr) {
+	this._collection = arr ? arr : [];
+	this.reverse = function() {
+		this._collection = this._collection.reverse();
+	};
+	this.iterator = function() {
+		return new ArrayIterator(this._collection);
+	}
+	this.find = function(prop, value) {
+		var it = this.iterator();
+		var item;
+
+		// Returns the first item
+		if (prop == "" && !value && it.hasNext()) {
+			item = it.next();
+		}
+		if (item) {
+			it.purge();
+			it = null;
+			return item;
+		}
+
+		while (it.hasNext()) {
+			item = it.next();
+			if (item.hasOwnProperty(prop) && item[prop] == value)
+				return item;
+
+			if ((prop || value ) && (item == prop || item == value ))
+				return item;
+		}
+		it.purge();
+		it = null;
+		return null;
+	};
+	this.addItem = function(value) {
+		this._collection.push(value);
+	};
+	this.addItemAt = function(value, index) {
+		this._collection.splice(index, 0, value);
+	};
+	this.getItemAt = function(index) {
+		return this._collection[index];
+	};
+	this.removeItem = function(prop, value) {
+		var removeIndex = -1;
+		var it = this.iterator();
+		while (it.hasNext()) {
+			var item = it.next();
+			if (item.hasOwnProperty(prop) && item[prop] == value) {
+				removeIndex = it.index() - 1;
+				break;
+			}
+		}
+		it.purge();
+		it = null;
+		return this._collection.splice(removeIndex, 1);
+	};
+	this.removeItemAt = function(index) {
+		this._collection.splice(index, 1);
+	}
+
+	this.length = function() {
+		return this._collection ? this._collection.length : 0;
+	};
+
+}
